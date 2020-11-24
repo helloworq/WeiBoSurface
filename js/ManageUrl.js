7d5c95400b88047b3bb992fb758d2d8355e0ece7
周磊 <1273370082@qq.com>
2020-11-24 16:14:43 +0800

    新增快捷url设置

---
+++ js/ManageUrl.js
@@ -0,0 +1,15 @@
+var url_getFriendMoment_107 = 'http://192.168.1.107:6729/v1/rest/getFriendMoment'
+var url_getFriendMoment_223 = 'http://192.168.2.223:6729/v1/rest/getFriendMoment'
+
+var url_getMainPageInfo_107 = 'http://192.168.1.107:6729/v1/rest/getMainPageInfo'
+var url_getMainPageInfo_223 = 'http://192.168.2.223:6729/v1/rest/getMainPageInfo'
+
+var url_fileUpload_107 = 'http://192.168.1.107:6729/v1/rest/fileUpload'
+var url_fileUpload_223 = 'http://192.168.2.223:6729/v1/rest/fileUpload'
+
+var url_like_107 = 'http://192.168.1.107:6729/v1/rest/like'
+var url_like_223 = 'http://192.168.2.223:6729/v1/rest/like'
+
+var url_collect_107 = 'http://192.168.1.107:6729/v1/rest/collect'
+var url_collect_223 = 'http://192.168.2.223:6729/v1/rest/collect'
+
